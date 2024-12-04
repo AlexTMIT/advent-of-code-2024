59b10802ccbf89a466846a22f27c1d6b88e93f30
Alex Tilgreen Mogensen <alext@itu.dk>
2024-12-04 07:16:10 +0100

    major cleanup

--- src/04/main.go
+++ src/04/main.go
@@ -7,59 +7,65 @@ import (
 	"strings"
 )
 
-var n = 140 // 10 for test, 140 for input
-var a = make([][]string, n)
-var count = 0
+const n = 140 // 10 for test, 140 for input
+
+var grid = make([][]string, n)
 
 func main() {
-	file, _ := os.Open("input.txt")
+	readInput("input.txt")
+	count := countOccurrences()
+	log.Printf("Solution 1: %d\n", count)
+}
+
+func readInput(filename string) {
+	file, _ := os.Open(filename)
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	i := 0
+	row := 0
 	for scanner.Scan() {
-		parts := strings.Split(scanner.Text(), "")
-		a[i] = parts
-		i++
+		grid[row] = strings.Split(scanner.Text(), "")
+		row++
+	}
+}
+
+func countOccurrences() int {
+	count := 0
+	directions := []struct{ dx, dy int }{
+		{1, 0},   // right
+		{-1, 0},  // left
+		{0, 1},   // down
+		{0, -1},  // up
+		{-1, 1},  // bottom left
+		{-1, -1}, // top left
+		{1, 1},   // bottom right
+		{1, -1},  // top right
 	}
 
-	for y, s := range a {
-		for x, c := range s {
-			if c == "X" {
-				log.Printf("Checking %s in %d, %d\n", c, x, y)
-
-				if x+3 < n && a[y][x+1] == "M" && a[y][x+2] == "A" && a[y][x+3] == "S" { // right
-					count++
-				}
-				if x-3 >= 0 && a[y][x-1] == "M" && a[y][x-2] == "A" && a[y][x-3] == "S" { // left
-					count++
-				}
-				if y+3 < n && a[y+1][x] == "M" && a[y+2][x] == "A" && a[y+3][x] == "S" { // down
-					count++
-				}
-
-				if y-3 >= 0 && a[y-1][x] == "M" && a[y-2][x] == "A" && a[y-3][x] == "S" { // up
-					count++
-				}
-
-				if y+3 < n && x-3 >= 0 && a[y+1][x-1] == "M" && a[y+2][x-2] == "A" && a[y+3][x-3] == "S" { // bottom left
-					count++
-				}
-
-				if y-3 >= 0 && x-3 >= 0 && a[y-1][x-1] == "M" && a[y-2][x-2] == "A" && a[y-3][x-3] == "S" { // top left
-					count++
-				}
-
-				if y+3 < n && x+3 < n && a[y+1][x+1] == "M" && a[y+2][x+2] == "A" && a[y+3][x+3] == "S" { // bottom right
-					count++
-				}
-
-				if y-3 >= 0 && x+3 < n && a[y-1][x+1] == "M" && a[y-2][x+2] == "A" && a[y-3][x+3] == "S" { // top right
-					count++
+	for y, row := range grid {
+		for x, char := range row {
+			if char == "X" {
+				for _, dir := range directions {
+					if matchesPattern(x, y, dir.dx, dir.dy) {
+						count++
+					}
 				}
 			}
 		}
 	}
 
-	log.Println(count)
+	return count
+}
+
+func matchesPattern(x, y, dx, dy int) bool {
+	if !isValidPosition(x+3*dx, y+3*dy) {
+		return false
+	}
+	return grid[y+dy][x+dx] == "M" &&
+		grid[y+2*dy][x+2*dx] == "A" &&
+		grid[y+3*dy][x+3*dx] == "S"
+}
+
+func isValidPosition(x, y int) bool {
+	return x >= 0 && x < n && y >= 0 && y < n
 }
