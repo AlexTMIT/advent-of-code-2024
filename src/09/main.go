00038777e8cb1bd9b8839ab31f424f48cdc862a5
Alex Tilgreen Mogensen <alext@itu.dk>
2024-12-09 11:13:14 +0100

    day 9, part 2

--- src/09/main.go
+++ src/09/main.go
@@ -9,15 +9,22 @@ import (
 )
 
 var count1 = 0
-var blocks = make([]int, 0)
-var empty = make([]int, 0)
-var lim = 0
+var count2 = 0
+
+var Blocks = make([]int, 0)
+var Empty = make([]int, 0)
+var indexToFirstEmpty = make([]int, 0)
 
 func main() {
 	readInput("input.txt")
-	reorder()
-	calculateChecksum()
+
+	blocksOne := partOne()
+	calculateChecksum(blocksOne, &count1)
+	blocksTwo := partTwo()
+	calculateChecksum(blocksTwo, &count2)
+
 	log.Printf("Solution 1: %d", count1)
+	log.Printf("Solution 2: %d", count2)
 }
 
 func readInput(filename string) {
@@ -35,14 +42,15 @@ func readInput(filename string) {
 
 			if isFile {
 				for range num {
-					blocks = append(blocks, nextID)
+					Blocks = append(Blocks, nextID)
 					index++
 				}
 				nextID++
 			} else {
+				indexToFirstEmpty = append(indexToFirstEmpty, index)
 				for range num {
-					blocks = append(blocks, -1)
-					empty = append(empty, index)
+					Blocks = append(Blocks, num*-1)
+					Empty = append(Empty, index)
 					index++
 				}
 			}
@@ -52,34 +60,87 @@ func readInput(filename string) {
 	}
 }
 
-func reorder() {
+func partOne() []int {
+	var blocks = make([]int, len(Blocks))
+	copy(blocks, Blocks)
+
 	iFile := len(blocks) - 1
 
-	for _, iEmpty := range empty {
+	for _, iEmpty := range Empty {
 		if iFile <= iEmpty {
 			break
 		}
 
-		for iFile >= 0 && blocks[iFile] == -1 {
+		for iFile >= 0 && blocks[iFile] < 0 {
 			iFile--
 		}
 
 		if iFile >= 0 {
 			blocks[iEmpty] = blocks[iFile]
-			blocks[iFile] = -1
+			blocks[iFile] = -11
 			iFile--
 		}
 	}
 
-	lim = iFile
+	return blocks
 }
 
-func calculateChecksum() {
-	for i, num := range blocks {
-		if i > lim {
-			break
+func partTwo() []int {
+	var blocks = make([]int, len(Blocks))
+	copy(blocks, Blocks)
+
+	emptySpaces := make([][2]int, 0) // [0] = index, [1] = space
+	for _, iEmpty := range indexToFirstEmpty {
+		emptySpaces = append(emptySpaces, [2]int{iEmpty, blocks[iEmpty] * -1})
+	}
+
+	for iFile := len(blocks) - 1; iFile >= 0; {
+		if blocks[iFile] < 0 {
+			iFile--
+			continue
 		}
 
-		count1 += i * num
+		fileID := blocks[iFile]
+		fileStart := iFile
+		for fileStart >= 0 && blocks[fileStart] == fileID {
+			fileStart--
+		}
+		fileStart++
+
+		fileLength := iFile - fileStart + 1
+
+		for i, e := range emptySpaces { // find first empty space that fits file
+			iEmpty, emptyLength := e[0], e[1]
+
+			if fileLength <= emptyLength && iEmpty < fileStart { // file fits?
+				for j := 0; j < fileLength; j++ { // move file
+					blocks[iEmpty+j] = fileID
+					blocks[fileStart+j] = -11
+				}
+
+				if fileLength < emptyLength { // update empty space tracking
+					newEmptyStart := iEmpty + fileLength
+					newEmptyLength := emptyLength - fileLength
+					blocks[newEmptyStart] = -newEmptyLength
+					emptySpaces[i] = [2]int{newEmptyStart, newEmptyLength}
+				} else {
+					emptySpaces = append(emptySpaces[:i], emptySpaces[i+1:]...)
+				}
+				break
+			}
+		}
+
+		iFile = fileStart - 1
+	}
+
+	return blocks
+}
+
+func calculateChecksum(blocks []int, count *int) {
+	*count = 0
+	for i, num := range blocks {
+		if num >= 0 {
+			*count += i * num
+		}
 	}
 }
