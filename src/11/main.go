6b5e244ff944c6ef078d440b5022313dc2bcd579
Alex Tilgreen Mogensen <alext@itu.dk>
2024-12-11 07:48:54 +0100

    day 11! :)

---
+++ src/11/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"strconv"
+)
+
+var stones = map[int]int{
+	3028:    1,
+	78:      1,
+	973951:  1,
+	5146801: 1,
+	5:       1,
+	0:       1,
+	23533:   1,
+	857:     1,
+}
+
+func main() {
+	blink(75) // limit seems to be 124
+	log.Printf("Solution: %d\n", count())
+}
+
+func blink(n int) {
+	for i := 0; i < n; i++ {
+		nextStones := make(map[int]int)
+		for num, count := range stones {
+			evolveStone(num, count, nextStones)
+		}
+
+		stones = nextStones
+	}
+}
+
+func evolveStone(num, count int, result map[int]int) {
+	if num == 0 {
+		result[1] += count
+		return
+	}
+
+	sNum := strconv.Itoa(num)
+	if len(sNum)%2 == 0 {
+		mid := len(sNum) / 2
+		left, _ := strconv.Atoi(sNum[:mid])
+		right, _ := strconv.Atoi(sNum[mid:])
+		result[left] += count
+		result[right] += count
+	} else {
+		xmasNum := num * 2024
+		result[xmasNum] += count
+	}
+}
+
+func count() (total int) {
+	for _, count := range stones {
+		total += count
+	}
+	return
+}
